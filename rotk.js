56ae6b14f24fe987768478d00485a16c48825dec
Guan Zhang <guanzhang@ROTK.local>
2018-10-23 23:33:36 -0700

    Added code for file management; fix bug with unregister & double registrations

--- rotk.js
+++ rotk.js
@@ -2,22 +2,42 @@
 
 var Discord = require('discord.io');
 var auth = require('./auth.json');
+var fs = require("fs");
+let botname = "ROTKbot";
 let participants = [];
 let nextRaidDate = "10/25/2018"
 let nextRaidTime = "+21";
+let raidFile = "data/nextRaid.json";
 var msg = "";
 
 // Init ROTKbot
+console.log("Initializing " + botname);
 var bot = new Discord.Client({
   token: auth.token,
   autorun: true
 });
 
 bot.on('ready', function (evt) {
-  console.log("[" + bot.username + "] id: " + bot.id);
+  console.log(botname + " [" + bot.username + "] id: " + bot.id + " ready");
+});
+
+// Load nextRaid.json file
+console.log("Opening file: " + raidFile);
+fs.exists(raidFile, function(exists) {
+  if (exists) { 
+    fs.readFile(raidFile, function (err, data) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("Reading file: " + raidFile);
+     })
+  } else {
+      console.log("File: " + raidFile + " does not exist");
+  }
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
+  var msg = "";
   // Bot will listen on '!' commands
   if (message.substring(0, 1) == '!') {
      var args = message.substring(1).split(' ');
@@ -27,16 +47,24 @@ bot.on('message', function (user, userID, channelID, message, evt) {
      switch(cmd) {
         // Register for raid
         case 'register':
-           participants.push(userID);
+           var found = participants.find(function(name) {
+             return name == userID;
+           });
+           if (!found) {
+             participants.push(userID);
+             msg = "You are registered for the next raid scheduled for "+nextRaidDate+ " at "+nextRaidTime+" hours";
+           } else {
+             msg = "You are already registered for the next raid scheduled for "+nextRaidDate+ " at "+nextRaidTime+" hours"     
+           };
            bot.sendMessage({
-              to: channelID,
-              message: "You are registered for the next raid scheduled for "+nextRaidDate+ " at "+nextRaidTime+" hours"
+             to: channelID,
+              message: msg
            });
         break;
 
         // Unregister from raid
         case 'unregister':
-           participants.pop(userID);
+           participants = participants.filter(u => u != userID);
            bot.sendMessage({
               to: channelID,
               message: "You have been unregistered from the next raid"

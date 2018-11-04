95e63e5bb018a3cc9c20586621beb3b75c31952a
Guan Zhang <guanzhang@ROTK.local>
2018-11-03 23:56:45 -0700

    Same thing for !damage

--- rotk.js
+++ rotk.js
@@ -89,6 +89,8 @@ function printDamage(msg, obj, evt) {
     // Prints server-specific nickname, if set
     if (userObj && userObj.nick != null) {
       username = bot.servers[serverID].members[obj[key].name].nick;
+    } else if (bot.users[obj[key].name] == undefined) {
+      username = "Unknown";
     } else {
       username = bot.users[obj[key].name].username;
     }
